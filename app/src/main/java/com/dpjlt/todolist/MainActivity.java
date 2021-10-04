99ce4a9680a088a13ac4a32a10397aeeab68adfc
HeraclitusDEF <davebodruzzaman@gmail.com>
2021-10-04 21:42:04 +0100

    ui: Button Added

--- app/src/main/java/com/dpjlt/todolist/MainActivity.java
+++ app/src/main/java/com/dpjlt/todolist/MainActivity.java
@@ -1,8 +1,10 @@
 package com.dpjlt.todolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onBtnClick (View view){
+        TextView txtHello = findViewById(R.id.textJack);
+        txtHello.setText("Hi Jack");
+
+    }
+
 }

a70315098a4987713e0b6b617ba8a7b20684f7f3
JackPatrickBlaiseBrennan <jackpbbrennan@gmail.com>
2021-11-08 22:34:49 +0000

    JAVA: Adding fully functional

--- app/src/main/java/com/dpjlt/todolist/MainActivity.java
+++ app/src/main/java/com/dpjlt/todolist/MainActivity.java
@@ -1,40 +1,62 @@
 package com.dpjlt.todolist;
 
-import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-    // todo make private and access with getter
     private static ToDoList toDoList = AppLaunch.getToDoList();
-
-    private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
-    private EditText newTaskPopup;
-    private Button newTaskSaveButton;
+    public static TodoItemsAdapter mTodoListAdapter = new TodoItemsAdapter(toDoList);
+    private EditText editTodo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toDoList.addItem("WOW");
-
-        RecyclerView rvTodoList = findViewById(R.id.recyclerView);
+        RecyclerView rvTodoList = findViewById(R.id.rvTodoList);
         //bind the recyclerview
-        TodoItemsAdapter adapter = new TodoItemsAdapter(toDoList);
-        rvTodoList.setAdapter(adapter);
+        rvTodoList.setAdapter(mTodoListAdapter);
         rvTodoList.setLayoutManager(new LinearLayoutManager(this));
+
+        this.setupEditTextListener();
+        // start up task for testing : )
+        toDoList.addItem("WOW");
     }
 
     public void addTask (View view){
-        EditText editTodo = findViewById(R.id.editTodo);
         toDoList.addItem(editTodo.getText().toString());
         editTodo.setText("");
+        // close the keyboard
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
+
+    public void removeTask (View view){
+//        toDoList.removeItem();
+    }
+
+    private void setupEditTextListener(){
+        editTodo = findViewById(R.id.editTodo);
+        editTodo.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    addTask(textView);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
 }

0d9b8246123fed68e2d96cd579fd089ad601ef6b
liam <llenihan21@gmail.com>
2021-11-10 13:40:33 +0000

    Added javadoc to AppLaunch

--- app/src/main/java/com/dpjlt/todolist/AppLaunch.java
+++ app/src/main/java/com/dpjlt/todolist/AppLaunch.java
@@ -3,9 +3,16 @@ package com.dpjlt.todolist;
 import android.app.Application;
 import android.util.Log;
 
+/**
+ * Launches the application
+ */
 public class AppLaunch extends Application {
     private static ToDoList toDoList;
 
+    /**
+     * create the list of todos
+     * todo add the database stuff here too
+     */
     public AppLaunch(){
         toDoList = new ToDoList();
         Log.i("main", "Constructor fired");

41057946842141135a25da806e387fac65fdbf92
JackPatrickBlaiseBrennan <jackpbbrennan@gmail.com>
2021-11-26 17:30:59 +0000

    Added Two Black Box Tests

--- app/src/androidTest/java/com/dpjlt/todolist/BlackBoxTests.java
+++ app/src/androidTest/java/com/dpjlt/todolist/BlackBoxTests.java
@@ -1,6 +1,7 @@
 package com.dpjlt.todolist;
 
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -11,7 +12,10 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.hasToString;
 
 import android.widget.DatePicker;
 
@@ -48,13 +52,12 @@ public class BlackBoxTests {
 
     @Test
     public void editTaskHeaderText() {
-        String taskHeader = "Test Task";
+        String taskHeaderMaxChar = "FSFFASFADSSAFSADFDSADSADSAFDSAADSFFDASSFDASFFDSDSFADSF";
         onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.name))
-                .perform(typeText(taskHeader), closeSoftKeyboard());
+                .perform(typeText(taskHeaderMaxChar), closeSoftKeyboard());
         onView(withId(R.id.name))
-                .check(matches(withText(taskHeader)));
-
+                .check(matches(withText(taskHeaderMaxChar.substring(0, 20))));
     }
     @Test
     public void editTaskDueDate() {
@@ -64,6 +67,36 @@ public class BlackBoxTests {
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(1900, 10, 28));
         onView(withId(android.R.id.button1)).perform(click());
         onView(withId(R.id.due_date)).check(matches(withText(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)+"/"+ (Calendar.getInstance().get(Calendar.MONTH)+1)+"/"+Calendar.getInstance().get(Calendar.YEAR))));
+    }
+    @Test
+    public void editTagText() {
+        String taskHeaderMaxChar = "FSFFASFADSSAFS";
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.tag))
+                .perform(typeText(taskHeaderMaxChar), closeSoftKeyboard());
+        onView(withId(R.id.tag))
+                .check(matches(withText(taskHeaderMaxChar.substring(0, 10))));
+    }
+    @Test
+    public void editPioritySpinner() {
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.priority)).perform(click());
+        onData(hasToString("Low")).perform(click());
+        onView(withId(R.id.priority))
+                .check(matches(withSpinnerText("Low")));
+        onView(withId(R.id.priority)).perform(click());
+        onData(hasToString("Medium")).perform(click());
+        onView(withId(R.id.priority))
+                .check(matches(withSpinnerText("Medium")));
+        onView(withId(R.id.priority)).perform(click());
+        onData(hasToString("High")).perform(click());
+        onView(withId(R.id.priority))
+                .check(matches(withSpinnerText("High")));
+        onView(withId(R.id.priority)).perform(click());
+        onData(hasToString("No Priority")).perform(click());
+        onView(withId(R.id.priority))
+                .check(matches(withSpinnerText("No Priority")));
+
     }
 
     }
